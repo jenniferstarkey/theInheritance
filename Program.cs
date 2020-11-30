f5912993710283b0e04469e87f044331f7d2e1d9
jenniferstarkey <jenniferrstarkey@yahoo.com>
2020-11-30 10:19:06 -0600

    vehicles returning correct noises

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace theInheritance
+namespace Garage
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Zero fxs = new Zero();
+            Tesla modelS = new Tesla();
+            Cessna mx410 = new Cessna();
+            Ram model1500 = new Ram();
+
+            fxs.Drive();
+            modelS.Drive();
+            mx410.Drive();
+            model1500.Drive();
         }
     }
 }
---
+++ Tesla.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Tesla : Vehicle // Electric car
+{
+    public double BatteryKWh { get; set; }
+    public void ChargeBattery()
+    {
+        // method definition omitted
+    }
+}
